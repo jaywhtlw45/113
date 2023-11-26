99290122c5510ef0e4e05d1d1c2a6726b86f6c7d
Jason Whitlow <jason.whitlow45@gmail.com>
2023-11-25 16:58:43 -0800

    working in accessmemory()

--- memory/main.cpp
+++ memory/main.cpp
@@ -10,6 +10,8 @@ using namespace std;
 struct CacheBlock
 {
     bool valid;
+    bool coldStart;
+    bool history;
     bitset<4> tag;
     int data;
 };
@@ -27,18 +29,21 @@ vector<vector<CacheBlock>> cache(CACHE_SIZE / CACHE_ASSOC, vector<CacheBlock>(CA
 
 // Function prototypes
 void initializeMemory();
-void initializeCache();
-
 void displayCache();
 void displayMemory();
-void accessMemory(int address, bool write);
+void accessMemory(bitset<16> address, bool write);
 
 void processInstruction(bitset<BITS> instruction);
+
 bitset<BITS> stringToBitset(string line);
 void readFile();
 
 // Register file
 int registers[8] = {0};
+
+// Cache bits
+int coldStartBit[8] = {0};
+int historyBit[8] = {0};
 
 // Main memory
 int memory[MEM_SIZE];
@@ -49,8 +54,6 @@ int main()
     initializeMemory();
     readFile();
     displayCache();
-
-    
 
     // Display final contents
     // displayCache();
@@ -63,56 +66,101 @@ int main()
 // '100011' for load and '101011' for store
 void processInstruction(bitset<BITS> instruction)
 {
-    bitset<6> opcode;   // bits 26-31
-    for(int i = 0; i < 6; i++) {
-        opcode[i] = instruction[i+26];
+    bitset<6> opcode; // bits 26-31
+    for (int i = 0; i < 6; i++)
+    {
+        opcode[i] = instruction[i + 26];
     }
-    bitset<5> rs;       // bits 21-25
-    for(int i= 0; i < 5; i++) {
-        rs[i] = instruction[i+21];
-    }
-    bitset<5> rt;       // bits 16-20
-    for(int i = 0; i < 5; i++) {
-        rt[i] = instruction[i+16];
-    }
-    bitset<16> offset;  // bits 0-15
-    for(int i = 0; i < 16; i++) {
-        offset[i] = instruction[i];
+    bitset<16> address;
+    for (int i = 0; i < 16; i++)
+    {
+        address[i] = instruction[i];
     }
 
-    if (opcode == 35) {
-        // load
-        cout << "load" << endl;
-        cout << "rs " << rs << endl;
-        cout << "rt " << rt << endl;
-        cout << "offset " << offset << endl;
-    } else if (opcode == 43) {
-        // store
-        cout << "store" << endl;
-        cout << "rs " << rs << endl;
-        cout << "rt " << rt << endl;
-        cout << "offset " << offset << endl;
-    } else {
-        cout << "opcode " << opcode << endl;
+    int int_address = address.to_ulong();
+    cout << "int_address " << int_address << endl;
+    // load
+    if (opcode == 35)
+    {
+        accessMemory(address, 0);
+    }
+    // store
+    else if (opcode == 43)
+    {
+        accessMemory(address, 1);
+    }
+    else
+    {
+        cout << "error" << endl;
+        exit(1);
     }
 }
 
-bitset<BITS> stringToBitset(string line)
+int getIndex(bitset<16> address)
 {
-    bitset<BITS> instruction;
-    for (int i = 0; i < BITS; i++)
+    bitset<3> index;
+    for (int i = 0; i < 4; i++)
+        index[i] = address[i];
+
+    return index.to_ulong();
+}
+
+bitset<4> getTag(bitset<16> address)
+{
+    bitset<4> tag;
+    for (int i = 3; i < 7; i++)
+        tag[i-3] = address[i];
+
+    return tag;
+}
+// Function to simulate memory access
+void accessMemory(bitset<16> address, bool write)
+{
+    cout << "address " << address << endl;
+    // Extract index and tag from the address
+    int index = getIndex(address);
+    cout << "index " << index << endl;
+
+    bitset<4> tag = getTag(address);
+    cout << "tag " << tag << endl;
+
+    int int_address = address.to_ulong();
+
+    // Check if the data is in the cache
+    bool cacheHit = false;
+    int way = -1;
+    for (int i = 0; i < CACHE_ASSOC; ++i)
     {
-        if (line[i] == '1')
+        if (cache[index][i].valid && cache[index][i].tag == tag)
         {
-            instruction.set(BITS - i - 1);
+            cacheHit = true;
+            way = i;
+            break;
         }
     }
-    return instruction;
-}
 
-void initializeCache()
-{
+    // Display hit or miss message
+    cout << (cacheHit ? "Hit" : "Miss") << " at address " << int_address << endl;
 
+    // If it's a cache miss, update the cache
+    if (!cacheHit)
+    {
+        // Find the LRU way
+        way = (cache[index][0].valid && cache[index][1].valid) ? 0 : 1;
+
+        // Update the cache block
+        cache[index][way].valid = true;
+        cache[index][way].tag = tag;
+        cache[index][way].data = memory[int_address >> 2];
+    }
+
+    // If it's a write operation, update the data in the cache and memory
+    if (write)
+    {
+        int regIndex = (int_address >> 2) & 0x07; // 3 bits for register index
+        cache[index][way].data = registers[regIndex];
+        memory[int_address >> 2] = registers[regIndex];
+    }
 }
 
 void readFile()
@@ -137,6 +185,17 @@ void readFile()
     inputFile.close();
 }
 
+// Function to display main memory contents
+void displayMemory()
+{
+    cout << "Memory Contents:" << endl;
+    for (int i = 0; i < MEM_SIZE; ++i)
+    {
+        cout << "Block " << i << ": " << memory[i] << endl;
+    }
+    cout << endl;
+}
+
 // Function to initialize memory
 void initializeMemory()
 {
@@ -157,11 +216,13 @@ void displayCache()
             cout << "Set " << i << ", Way " << j << ": ";
             if (cache[i][j].valid)
             {
-                cout << "V:1 Tag:" << cache[i][j].tag << " Data:" << cache[i][j].data;
+                cout << "CS:" << cache[i][j].coldStart << " H:" << cache[i][j].history;
+                cout << " V:1 Tag:" << cache[i][j].tag << " Data:" << cache[i][j].data;
             }
             else
             {
-                cout << "V:0";
+                cout << "CS:" << cache[i][j].coldStart << " H:" << cache[i][j].history;
+                cout << " V:0";
             }
             cout << endl;
         }
@@ -169,74 +230,29 @@ void displayCache()
     cout << endl;
 }
 
-// Function to display main memory contents
-void displayMemory()
+bitset<BITS> stringToBitset(string line)
 {
-    cout << "Memory Contents:" << endl;
-    for (int i = 0; i < MEM_SIZE; ++i)
+    bitset<BITS> instruction;
+    for (int i = 0; i < BITS; i++)
     {
-        cout << "Block " << i << ": " << memory[i] << endl;
-    }
-    cout << endl;
-}
-
-// Function to simulate memory access
-void accessMemory(int address, bool write)
-{
-    // Extract index and tag from the address
-    int index = (address >> 2) & 0x07; // 3 bits for index
-    bitset<4> tag(address >> 5);       // 4 bits for tag
-
-    // Check if the data is in the cache
-    bool cacheHit = false;
-    int way = -1;
-    for (int i = 0; i < CACHE_ASSOC; ++i)
-    {
-        if (cache[index][i].valid && cache[index][i].tag == tag)
+        if (line[i] == '1')
         {
-            cacheHit = true;
-            way = i;
-            break;
+            instruction.set(BITS - i - 1);
         }
     }
-
-    // Display hit or miss message
-    cout << (cacheHit ? "Hit" : "Miss") << " at address " << address << endl;
-
-    // If it's a cache miss, update the cache
-    if (!cacheHit)
-    {
-        // Find the LRU way
-        way = (cache[index][0].valid && cache[index][1].valid) ? 0 : 1;
-
-        // Update the cache block
-        cache[index][way].valid = true;
-        cache[index][way].tag = tag;
-        cache[index][way].data = memory[address >> 2];
-    }
-
-    // If it's a write operation, update the data in the cache and memory
-    if (write)
-    {
-        int regIndex = (address >> 2) & 0x07; // 3 bits for register index
-        cache[index][way].data = registers[regIndex];
-        memory[address >> 2] = registers[regIndex];
-    }
+    return instruction;
 }
 
-
-
-
 // Input instruction sequence
-    // int instructions[] = {0x8D010004, 0x8D020010, 0x8D030020, 0x8D040014,
-    //                       0xAD010050, 0xAD020044, 0xAD03004C, 0xAD0400E0,
-    //                       0x8D010024, 0x8D02002C, 0x8D030010, 0x8D0400AC,
-    //                       0xAD010014, 0xAD020018, 0xAD030024, 0xAD040044,
-    //                       0x8D010024, 0x8D02002C, 0x8D030010, 0x8D0400AC,
-    //                       0xAD010060, 0xAD020054, 0xAD03005C, 0xAD0400F0};
+// int instructions[] = {0x8D010004, 0x8D020010, 0x8D030020, 0x8D040014,
+//                       0xAD010050, 0xAD020044, 0xAD03004C, 0xAD0400E0,
+//                       0x8D010024, 0x8D02002C, 0x8D030010, 0x8D0400AC,
+//                       0xAD010014, 0xAD020018, 0xAD030024, 0xAD040044,
+//                       0x8D010024, 0x8D02002C, 0x8D030010, 0x8D0400AC,
+//                       0xAD010060, 0xAD020054, 0xAD03005C, 0xAD0400F0};
 
-    // Simulate memory operations
-    // for (int i = 0; i < sizeof(instructions) / sizeof(instructions[0]); ++i) {
-    //     cout << "instructions " << (instructions[i] & 0x0000FFFF) << endl;
-    //     accessMemory(instructions[i] & 0x0000FFFF, (instructions[i] >> 29) & 0x1);
-    // }
+// Simulate memory operations
+// for (int i = 0; i < sizeof(instructions) / sizeof(instructions[0]); ++i) {
+//     cout << "instructions " << (instructions[i] & 0x0000FFFF) << endl;
+//     accessMemory(instructions[i] & 0x0000FFFF, (instructions[i] >> 29) & 0x1);
+// }
